7ca2fc3aa7db5844bb8ed2d27cbfa38f6ae52f93
acodyjr <acody@me.com>
2016-03-14 12:35:13 +0000

    New and updated tasks for Week 8 warmup.

---
+++ task_01.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Collect user's name from input."""
+
+MY_ANSWER = raw_input('What is your name? ')
---
+++ task_02.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Collect user's age as an integer."""
+
+MY_INTEGER = int(raw_input('How old are you? '))
