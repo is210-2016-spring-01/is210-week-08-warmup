d48d62e9e8fc54a199ac31557cad98e0712bb928
paisely65 <cunysps@gmail.com>
2016-03-15 16:43:19 -0400

    Task i revised constant changed.

--- task_01.py
+++ task_01.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 """This docstring uses raw_input to collect user input."""
 
-MY_NAME = raw_input("Who will be the next president? ")
+MY_ANSWER = raw_input("Who will be the next president? ")
 
 
