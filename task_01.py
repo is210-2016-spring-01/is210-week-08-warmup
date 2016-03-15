e45d2f8e9243c2b376af54feffcb7b35f2e88db7
paisely65 <cunysps@gmail.com>
2016-03-15 16:38:01 -0400

    Task 1 is finished

--- task_01.py
+++ task_01.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 """This docstring uses raw_input to collect user input."""
 
-MY_NAME == raw_input("Who will be the next president? ")
-Who will be the next president? Donald Trump
+MY_NAME = raw_input("Who will be the next president? ")
+
 
