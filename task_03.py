ac97c656fc7a2de4160499a232e165a2538319ca
William Cole <billy.cole@gmail.com>
2016-03-14 22:11:12 -0400

    sacrifice

--- task_03.py
+++ task_03.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A complex testcase."""
+"""TASK 3 SACRIFICE"""
 
 EXPENSE = 14.23
 LOOKS_NICE = True
 MAX_EXPENSE = 12
 GET_OUT_ALIVE = False
+
+SACRIFICE = (LOOKS_NICE and EXPENSE <= MAX_EXPENSE) or not GET_OUT_ALIVE
