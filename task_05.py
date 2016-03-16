1b7f88efabdce8e0129b65eddd7395d93e0099db
paisely65 <cunysps@gmail.com>
2016-03-15 21:48:18 -0400

    task 5 spaces fixed

--- task_05.py
+++ task_05.py
@@ -3,23 +3,19 @@
 """A  module to record patients' blood pressure"""
 
 BLOOD_PRESSURE = int(raw_input('What is patient blood pressure?'))
-BP_STATUS =''
+BP_STATUS = ''
 BLOOD_PRESSURE_RESULT = 'Your pressure is currently: {0}.'
 
 if BLOOD_PRESSURE <= 89:
-    BP_STATUS ='Low'
+    BP_STATUS = 'Low'
 elif BLOOD_PRESSURE >= 90 and BLOOD_PRESSURE <= 119:
-    BP_STATUS ='Ideal'
+    BP_STATUS = 'Ideal'
 elif BLOOD_PRESSURE >= 120 and BLOOD_PRESSURE <= 139:
-    BP_STATUS ='Warning'
+    BP_STATUS = 'Warning'
 elif BLOOD_PRESSURE >= 140 and BLOOD_PRESSURE <= 159:
-    BP_STATUS ='High'
+    BP_STATUS = 'High'
 else:
-    BP_STATUS ='Emergency'
+    BP_STATUS = 'Emergency'
 
 print BLOOD_PRESSURE_RESULT.format(BP_STATUS)
-
-
-
-
 
