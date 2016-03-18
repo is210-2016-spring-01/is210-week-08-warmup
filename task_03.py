23c5c9764137ee73000bc930b582983d710200dd
jaiz25 <jaizelle@gmail.com>
2016-03-18 03:54:49 +0000

    Task 3

--- task_03.py
+++ task_03.py
@@ -6,3 +6,8 @@ EXPENSE = 14.23
 LOOKS_NICE = True
 MAX_EXPENSE = 12
 GET_OUT_ALIVE = False
+
+if not (LOOKS_NICE and EXPENSE <= MAX_EXPENSE) or GET_OUT_ALIVE:
+    SACRIFICE = True
+else:
+    SACRIFICE = False
